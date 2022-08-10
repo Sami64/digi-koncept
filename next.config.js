5bc9b7d635c81d24a42051620e32e91865c29450
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-08-10 21:32:33 +0000

    ⚡️ update mapbox env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
 		domains: ["links.papareact.com", "images.unsplash.com"],
 	},
 	env: {
-		NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN:
-			process.env.NEXT_PUBLIC_MAPBOX_GL_ACCESS_TOKEN,
+		NEXT_PUBLIC_VERCEL_MAPBOX_GL_ACCESS_TOKEN:
+			process.env.NEXT_PUBLIC_VERCEL_MAPBOX_GL_ACCESS_TOKEN,
 	},
 };
