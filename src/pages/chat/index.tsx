22592932c277375a05b092d4756855ed000d4703
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-09-09 15:06:41 +0000

    ⚡️ second

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -11,7 +11,7 @@ const CometChatNoSSR = dynamic(() => import("../CometChatNoSSR"), {
 })
 
 const Chat = () => {
-	if (typeof window != undefined)
+	if (typeof window !== undefined)
 		window.CometChat = require("@cometchat-pro/chat").CometChat
 
 	return (
