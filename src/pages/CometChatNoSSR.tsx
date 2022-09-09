f4a705f076095ec2c33bec63db767db359476fd2
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-09-09 14:52:27 +0000

    ⚡️ retry cometchat

--- src/pages/CometChatNoSSR.tsx
+++ src/pages/CometChatNoSSR.tsx
@@ -1,42 +1,96 @@
+// import { CometChat } from "@cometchat-pro/chat"
+// import { useSession } from "next-auth/react"
+// import { useEffect } from "react"
+// import constants from "../core/utils/comet_constants"
+// import { CometChatUI } from "../cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/index"
+
 import { CometChat } from "@cometchat-pro/chat"
-import { useSession } from "next-auth/react"
-import { useEffect } from "react"
-import constants from "../core/utils/comet_constants"
+import { getSession } from "next-auth/react"
+import { Component } from "react"
 import { CometChatUI } from "../cometchat-pro-react-ui-kit/CometChatWorkspace/src/components/index"
+import constants from "../core/utils/comet_constants"
 
-const CometChatNoSSR = () => {
-	const { data: session, status } = useSession()
+// const CometChatNoSSR = () => {
+// 	const { data: session, status } = useSession()
 
-	const initCometChat = async () => {
-		const appSetting: CometChat.AppSettings = new CometChat.AppSettingsBuilder()
-			.subscribePresenceForAllUsers()
-			.setRegion(constants.REGION)
-			.autoEstablishSocketConnection(true)
-			.build()
+// 	const initCometChat = async () => {
+// 		const appSetting: CometChat.AppSettings = new CometChat.AppSettingsBuilder()
+// 			.subscribePresenceForAllUsers()
+// 			.setRegion(constants.REGION)
+// 			.autoEstablishSocketConnection(true)
+// 			.build()
 
-		const initResult = await CometChat.init(constants.APP_ID, appSetting)
-		console.log("Comet Initialization", initResult)
-		return initResult
-	}
+// 		const initResult = await CometChat.init(constants.APP_ID, appSetting)
+// 		console.log("Comet Initialization", initResult)
+// 		return initResult
+// 	}
 
-	const cometLogin = async () => {
-		try {
-			const user = await CometChat.login(session?.userId, constants.AUTH_KEY)
-			console.log("Comet Login user", user)
-		} catch (error) {
-			console.log("comet login error", error)
-		}
-	}
+// 	const cometLogin = async () => {
+// 		try {
+// 			const user = await CometChat.login(session?.userId, constants.AUTH_KEY)
+// 			console.log("Comet Login user", user)
+// 		} catch (error) {
+// 			console.log("comet login error", error)
+// 		}
+// 	}
 
-	useEffect(() => {
-		initCometChat().then((res) => cometLogin())
-	}, [])
+// 	useEffect(() => {
+// 		initCometChat().then((res) => cometLogin())
+// 	}, [])
 
-	return (
-		<div className=" w-screen h-screen">
-			<CometChatUI />
-		</div>
-	)
+// 	return (
+// 		<div className=" w-screen h-screen">
+// 			<CometChatUI />
+// 		</div>
+// 	)
+// }
+
+// export default CometChatNoSSR
+type MyProps = {}
+
+type MyState = {
+	user: any
 }
 
-export default CometChatNoSSR
+export default class CometChatNoSSR extends Component<MyProps, MyState> {
+	constructor(props: any) {
+		super(props)
+		this.state = { user: undefined }
+	}
+
+	componentDidMount(): void {
+		let appSetting = new CometChat.AppSettingsBuilder()
+			.subscribePresenceForAllUsers()
+			.setRegion(constants.REGION)
+			.build()
+		CometChat.init(constants.APP_ID, appSetting).then(
+			() => {
+				getSession().then((session) => {
+					CometChat.login(session?.userId, constants.AUTH_KEY).then(
+						(user) => {
+							this.setState({ user })
+						},
+						(error) => {
+							console.log("login failed wit exception:", { error })
+						}
+					)
+				})
+			},
+			(error) => {
+				console.log("Initialization failed with error")
+			}
+		)
+	}
+
+	render() {
+		if (this.state.user) {
+			return (
+				<div className=" w-screen h-screen">
+					<CometChatUI />
+				</div>
+			)
+		} else {
+			return <div>Loading....</div>
+		}
+	}
+}
--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,26 +1,26 @@
-import dynamic from "next/dynamic";
-import { useEffect } from "react";
+import dynamic from "next/dynamic"
+import { useEffect } from "react"
 
 declare global {
 	interface Window {
-		CometChat?: any;
+		CometChat?: any
 	}
 }
 
 const CometChatNoSSR = dynamic(() => import("../CometChatNoSSR"), {
 	ssr: false,
-});
+})
 
 const Chat = () => {
 	useEffect(() => {
-		window.CometChat = require("@cometchat-pro/chat").CometChat;
-	}, []);
+		window.CometChat = require("@cometchat-pro/chat").CometChat
+	})
 
 	return (
 		<div>
 			<CometChatNoSSR />
 		</div>
-	);
-};
+	)
+}
 
-export default Chat;
+export default Chat
