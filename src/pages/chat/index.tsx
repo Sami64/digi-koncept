424a77672ca86e2418e489db015fd9879024ca4c
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-09-15 11:57:43 +0000

    Revert "⚡️ try it again maan"
    This reverts commit 18df94bc0eeb33380b1b105e181a870153656e23.

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,21 +1,18 @@
 import dynamic from "next/dynamic"
-import { useEffect } from "react"
 
-// declare global {
-// 	interface Window {
-// 		CometChat?: any
-// 	}
-// }
+declare global {
+	interface Window {
+		CometChat?: any
+	}
+}
 
 const CometChatNoSSR = dynamic(() => import("../CometChatNoSSR"), {
 	ssr: false,
 })
 
 const Chat = () => {
-	useEffect(() => {
-		if (typeof window !== undefined)
-			window.CometChat = require("@cometchat-pro/chat").CometChat
-	}, [])
+	if (typeof window !== undefined)
+		window.CometChat = require("@cometchat-pro/chat").CometChat
 
 	return (
 		<div>
